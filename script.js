212632d657368dbc242d80c78d50f04cbabbfed7
Princepal <princepal.singh@thewitslab.com>
2022-02-02 11:42:04 +0530

    CHange in data

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
-
-//To select Input We Do This.
-
 const btn = document.querySelector('.btn');
-//passing Add Event Listener Here.
+var input = document.querySelector('input');
+var myDiv = document.querySelector('.todo1');
+const myDiv2 = document.querySelector('.todo2');
+
 btn.addEventListener('click',function myfunc(e){
-    e.preventDefault();
-    
-    // var array =[];
-    var input = document.querySelector('input');
-    var myDiv = document.querySelector('.todo1');
+
     var checkbox = document.createElement('input');
-    // checkbox.onclick = fun
     var label = document.createElement('label');
     var li = document.createElement('li');  
     checkbox.type = "checkbox";
 
-    // array.push(checkbox)
-    // console.log(array);
-    
+   
+
     var data = input.value;
     li.append(data);
     label.append(li);
     
     myDiv.append(checkbox);
-    myDiv.append(label);
-    console.log(label);
-    
-    input.value = " ";
+    myDiv.append(label);  
+    input.value = " ";   
 
-    // Implement Array to save Data.
-    // array.push()
-    
-    // var checkbox = document.querySelector('input');
-    
-    const myDiv2 = document.querySelector('.todo2');
-    checkbox.addEventListener('change',(e) => {
+
+    checkbox.addEventListener('click',(e) => {
         // var label1 = document.createElement('label');
         var list = document.createElement('li');
 
-        if(e.currentTarget.checked){
+        list.id = "hi";
 
-            var da = label.innerText;
-            list.append(da);
+
+        if(e.currentTarget.checked == true){
+            
+            // var da = label.innerText;
+            list.append(data);
             myDiv2.append(list);         
         }
-        else if(e.currentTarget ){
-            console.log(e)
-            // myDiv2.removeChild(list);
 
+        
+        else if(e.currentTarget.checked == false){
+            var lid = document.querySelector('#hi')
+            console.log(lid.innerText);
+            console.log(label.innerText);
             
-
-
+            if(label.innerText.id === lid.innerText.id){
+                lid.remove();
+            }
 
         }
     });
@@ -64,7 +57,7 @@ btn.addEventListener('click',function myfunc(e){
 
 
 
-{/* <input value="Click me" onclick="alert('Click!')" type="button"></input> */}
+/* <input value="Click me" onclick="alert('Click!')" type="button"></input> */
 
 
 
